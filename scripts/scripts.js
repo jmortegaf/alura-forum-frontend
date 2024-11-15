07dfe22af207821132c99686916211246a7d86d3
Jose Ortega <jmortegafreire@gmail.com>
2024-11-15 13:45:33 -0300

    bugs fix

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -52,7 +52,7 @@ function formatDateTime(localdatetime){
 function getValidToken(){
     const token=sessionStorage.getItem('jwt_token');
     // check if token is valid
-    if(token==null)window.location.href = `../login.html`;
+    if(token==null)window.location.href = 'login.html';
     else return token
 }
 function showThreadContent(state){
